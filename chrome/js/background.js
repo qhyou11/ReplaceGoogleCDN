c5cf5f0784eedfc1314c9536d87743d7581eaf79
qhyou11 <yunjianhe@gmail.com>
2019-04-01 10:53:03 +0800

    replace bootstrapcdn to cdn.bootcss.com

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -2,12 +2,14 @@ chrome.webRequest.onBeforeRequest.addListener(
     function(request) {
         var url = request.url.replace('googleapis.com', 'lug.ustc.edu.cn');
         url = url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
+        url = url.replace('maxcdn.bootstrapcdn.com', 'cdn.bootcss.com');
         return {redirectUrl: url};
     },
     {
         urls: [
             "*://ajax.googleapis.com/*",
-            "*://themes.googleusercontent.com/*"
+            "*://themes.googleusercontent.com/*",
+            "*://maxcdn.bootstrapcdn.com/*"
         ]
     },
     ["blocking"]
